50a1788696d98b9ad73e2936711e120f4e7c0e88
Aaryan Mishra <aaryan.mshra@gmail.com>
2024-05-23 19:06:26 +0530

    P

--- app.py
+++ app.py
@@ -38,15 +38,9 @@ def shorten():
 
     return jsonify({ 'shorten_url': shorten_url}), 201
 
-# @app.route('/<str:shorten_url>')
-# def redirect(shorten_url):
-#     shorten_url = "https://sfy.vercel.app/" + str(shorten_url)
-#
-
 @app.route('/<str:shorten_url>/delete', methods=['DELETE'])
 def delete(shorten_url):
-    # data = request.get_json()
-    return jsonify(shorten_url)
+    data = request.get_json()
     s_url = "https://sfy.vercel.app/" + str(shorten_url)
     if not data:
         return jsonify({"error": "No URL given"}), 400
